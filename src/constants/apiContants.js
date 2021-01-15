0298a87bf348af30c970d64167688d9548d28d83
Muhammad Mujir <muhammadmujir011@gmail.com>
2021-01-15 20:26:00 +0700

    update

--- src/constants/apiContants.js
+++ src/constants/apiContants.js
@@ -1,4 +1,4 @@
 //export const API_BASE_URL = process.env.REACT_APP_SERVER_URL;
-export const API_BASE_URL= process.env.NODE_ENV === 'development' ? 'http://localhost:8000/api' : 'http://api.hotelin.live:8000/api'
+export const API_BASE_URL= process.env.NODE_ENV === 'development' ? 'http://localhost:8000/api' : 'http://api.hotelin.live/api'
 export const ACCESS_TOKEN_NAME = 'login_access_token';
 export const IMAGE_URL= process.env.NODE_ENV === 'development' ? 'http://localhost:8000/storage/' : 'http://api.hotelin.live:8000/storage/'
